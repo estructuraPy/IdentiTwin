bffad01d4861f717dc4b4f79db2c580865bf8c03
ingahnavarro <102553393+ingahnavarro@users.noreply.github.com>
2025-04-27 02:16:17 -0600

    Update

--- examples/run_simulation.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Using Python virtual environment..."
-source ../setup_env/venv/bin/activate
-
-echo "Running Identitwin System in simulation mode..."
-python initialization.py --simulation
-
-read -p "Press Enter to continue..."
