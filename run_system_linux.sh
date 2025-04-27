cd44f4274f4a2584600f4258082884e0509e1625
ingahnavarro <102553393+ingahnavarro@users.noreply.github.com>
2025-04-27 03:41:45 -0600

    Fix: directory

---
+++ run_system_linux.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Activate the virtual environment using the current user's home directory
+source "$HOME/venv/bin/activate"
+
+# Run the python system script
+# Example: python main_system.py
